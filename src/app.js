689e7d9509036fc00fc354ee1ee2fd176ca92b20
Sylvia <smora1992@gmail.com>
2021-07-18 03:37:40 +0400

    change toapp.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+// const { NODE_ENV } = require('./config')
+const { CLIENT_ORIGIN } = require('./config')
 
 const app = express()
 
@@ -11,7 +12,9 @@ const morganOption = (process.env.NODE_ENV === 'production') ? 'tiny' : 'common'
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
