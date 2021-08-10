d17de3d2918ffeb2f71429b067fd6744681dfd89
Sylvia <smora1992@gmail.com>
2021-08-10 09:23:16 +0400

    changes to test files

--- test/restaurants-endpoints.spec.js
+++ test/restaurants-endpoints.spec.js
@@ -5,8 +5,9 @@ const app = require('../src/app')
 const { TEST_DATABASE_URL } = require('../src/config')
 const { makeRestaurantsArray } = require('./restaurants.seed')
 
-describe.only('Restaurants Endpoints', function () {
-    let db
+describe.only('Restaurants Service object', function () {
+    let db;
+    let testRestaurants = makeRestaurantsArray();
 
     before('make knex instance', () => {
         db = knex({
@@ -16,144 +17,148 @@ describe.only('Restaurants Endpoints', function () {
         app.set('db', db)
     })
 
-    after('disconnect from db', () => db.destroy())
-    before('clean the table', () => db('restaurants').truncate())
-    afterEach('cleanup', () => db('restaurants').truncate())
+
+    before(() => db('restaurants').truncate());
+
+    afterEach(() => db('restaurants').truncate());
+
+    after(() => db.destroy());
 
 
-
-
-    describe(`GET /api/restaurants`, () => {
-
-        context(`Given no restaurants`, () => {
-            it(`responds with 200 and an empty list`, () => {
-                return supertest(app)
-                    .get('/api/restaurants')
-                    .expect(200, [])
-            })
+    context(`Given no restaurants`, () => {
+        it(`responds with 200 and an empty list`, () => {
+            return supertest(app)
+                .get('/api/restaurants')
+                .expect(200, [])
         })
     })
-    context(`Given there are restaurants in the database`, () => {
+    context(`Given 'restaurants' has data`, () => {
         const testRestaurants = makeRestaurantsArray()
 
-        beforeEach('insertRestaurants', () => {
+        beforeEach('insert restaurants', () => {
             return db
                 .into('restaurants')
                 .insert(testRestaurants)
         })
         it('GET /api/restaurants responds with 200 and all the restaurants', () => {
+            const expectedRestaurants = testRestaurants.map(restaurant => ({
+                ...restaurant,
+            }));
             return supertest(app)
                 .get('/api/restaurants')
-                .expect(200, testRestaurants)
+                .expect(200, expectedRestaurants)
         })
+
     })
 
-
-    describe(`GET /restaurants/:id`, () => {
-        context('Given there are restaurants in the database', () => {
-            const testRestaurants = makeRestaurantsArray()
-
-            beforeEach('insert restaurants', () => {
-                return db
-                    .into('restaurants')
-                    .insert(testRestaurants)
-            })
-            it('GET /restaurants/:id responds with 200 and the specified restaurant', () => {
-                const restaurantId = 2
-                const expectedRestaurant = testRestaurants[restaurantId - 1]
-                return supertest(app)
-                    .get(`/restaurants/${restaurantId}`)
-                    .expect(200, expectedRestaurant)
-            })
-
-        })
-        context(`Given no restaurants`, () => {
-            it(`responds with 404`, () => {
-                const restaurantId = 123456
-                return supertest(app)
-                    .get(`/restaurants/${restaurantId}`)
-                    .expect(404, { error: { message: `restaurant doesn't exist` } })
-            })
-        })
-    })
-
-    describe.only(`POST /api/restaurants`, () => {
-        it(`creates a restaurant, responding with 201 and the new restaurant`, function () {
-            const requiredFields = [restaurant_name, description, rating, delivery_fee]
-
-            requiredFields.forEach(field => {
-
-                const newRestauraunt = {
-                    restaurant_name: 'Test new Restaurant',
-                    description: "food",
-                    rating: '3',
-                    delivery_fee: '100'
-                }
-                return supertest(app)
-                    .post('/api/restaurants')
-                    .send(newRestauraunt)
-                    .expect(201)
-                    .expect(res => {
-                        expect(res.body.restaurant_name).to.eql(newRestauraunt.restaurant_name)
-                        expect(res.body.description).to.eql(newRestaurant.description)
-                        expect(res.body.rating).to.eql(newRestaurant.rating)
-                        expect(res.body).to.have.property('id')
-                        expect(res.headers.location).to.eql(`/api/restaurants/${res.body.id}`)
-
-                    })
-                    .then(postRes =>
-                        supertest(app)
-                            .get(`/api/restaurants/${postRes.body.id}`)
-                            .expect(postRes.body)
-                    )
-            })
-
-            it(`responds with 400 and an error message when the '${field}' is missing`, () => {
-                delete newRestaurant[field]
-
-                return supertest(app)
-                    .post('/api/restaurants')
-                    .send(newRestaurant)
-                    .expect(400, {
-                        error: { message: `Missing '${field}' in request body` }
-                    })
-            })
-        })
-    })
-
-
-
-
-    describe.only(`DELETE /api/restaurants/:restaurant_id`, () => {
-        context('Given there are restaurants in the database', () => {
-            const testRestaurants = makeRestaurantsArray()
-
-            beforeEach('insert restaurants', () => {
-                return db
-                    .into('restaurants')
-                    .insert(testRestaurants)
-            })
-
-            it('responds with 204 and removes the restaurant', () => {
-                const idToRemove = 2
-                const expectedRestaurants = testRestaurants.filter(restaurant => restaurant.id !== idToRemove)
-                return supertest(app)
-                    .delete(`/api/restaurants/${idToRemove}`)
-                    .expect(204)
-                    .then(res =>
-                        supertest(app)
-                            .get(`/api/restaurants`)
-                            .expect(expectedRestaurants)
-                    )
-            })
-        })
-        context(`Given no restaurants`, () => {
-            it(`responds with 404`, () => {
-                const restaurantId = 123456
-                return supertest(app)
-                    .delete(`/restaurants/${restaurantId}`)
-                    .expect(404, { error: { message: `restaurant doesn't exist` } })
-            })
-        })
-    })
 })
+//testing id endpoints when id is UUID????
+
+//     describe(`GET /restaurants/:id`, () => {
+    //         context('Given there are restaurants in the database', () => {
+        //             const testRestaurants = makeRestaurantsArray()
+
+        //             beforeEach('insert restaurants', () => {
+            //                 return db
+            //                     .into('restaurants')
+            //                     .insert(testRestaurants)
+            //             })
+
+            // it('GET /api/restaurants/:id responds with 200 and the specified restaurant', () => {
+            //     const restaurantId = 2
+            //     const expectedRestaurant = testRestaurants[restaurantId - 1]
+            //     return supertest(app)
+            //         .get(`/restaurants/${restaurantId}`)
+            //         .expect(200, expectedRestaurant)
+            // })
+            //         })
+//         context(`Given no restaurants`, () => {
+//             it(`responds with 404`, () => {
+//                 const restaurantId = 123456
+//                 return supertest(app)
+//                     .get(`/restaurants/${restaurantId}`)
+//                     .expect(404, { error: { message: `restaurant doesn't exist` } })
+//             })
+//         })
+//     })
+
+//     describe.only(`POST /api/restaurants`, () => {
+//         it(`creates a restaurant, responding with 201 and the new restaurant`, function () {
+//             const requiredFields = [restaurant_name, description, rating, delivery_fee]
+
+//             requiredFields.forEach(field => {
+
+//                 const newRestauraunt = {
+//                     restaurant_name: 'Test new Restaurant',
+//                     description: "food",
+//                     rating: '3',
+//                     delivery_fee: '100'
+//                 }
+//                 return supertest(app)
+//                     .post('/api/restaurants')
+//                     .send(newRestauraunt)
+//                     .expect(201)
+//                     .expect(res => {
+//                         expect(res.body.restaurant_name).to.eql(newRestauraunt.restaurant_name)
+//                         expect(res.body.description).to.eql(newRestaurant.description)
+//                         expect(res.body.rating).to.eql(newRestaurant.rating)
+//                         expect(res.body).to.have.property('id')
+//                         expect(res.headers.location).to.eql(`/api/restaurants/${res.body.id}`)
+
+//                     })
+//                     .then(postRes =>
+//                         supertest(app)
+//                             .get(`/api/restaurants/${postRes.body.id}`)
+//                             .expect(postRes.body)
+//                     )
+//             })
+
+//             it(`responds with 400 and an error message when the '${field}' is missing`, () => {
+//                 delete newRestaurant[field]
+
+//                 return supertest(app)
+//                     .post('/api/restaurants')
+//                     .send(newRestaurant)
+//                     .expect(400, {
+//                         error: { message: `Missing '${field}' in request body` }
+//                     })
+//             })
+//         })
+//     })
+
+
+
+
+//     describe.only(`DELETE /api/restaurants/:restaurant_id`, () => {
+//         context('Given there are restaurants in the database', () => {
+//             const testRestaurants = makeRestaurantsArray()
+
+//             beforeEach('insert restaurants', () => {
+//                 return db
+//                     .into('restaurants')
+//                     .insert(testRestaurants)
+//             })
+
+//             it('responds with 204 and removes the restaurant', () => {
+//                 const idToRemove = 2
+//                 const expectedRestaurants = testRestaurants.filter(restaurant => restaurant.id !== idToRemove)
+//                 return supertest(app)
+//                     .delete(`/api/restaurants/${idToRemove}`)
+//                     .expect(204)
+//                     .then(res =>
+//                         supertest(app)
+//                             .get(`/api/restaurants`)
+//                             .expect(expectedRestaurants)
+//                     )
+//             })
+//         })
+//         context(`Given no restaurants`, () => {
+//             it(`responds with 404`, () => {
+//                 const restaurantId = 123456
+//                 return supertest(app)
+//                     .delete(`/restaurants/${restaurantId}`)
+//                     .expect(404, { error: { message: `restaurant doesn't exist` } })
+//             })
+//         })
+//     })
+// })
--- test/restaurants.seed.js
+++ test/restaurants.seed.js
@@ -1,19 +1,19 @@
 function makeRestaurantsArray() {
     return [
         {
-            "id": "001",
+            // "id": "001",
             "restaurant_name": "Burger King",
             "description": "Redundant alt attribute. Screen-readers already announce `img` tags as an image. You don’t need to use the words `image`, `photo,` or `picture` (or any specified custom words) in the alt prop",
             "rating": "4.8",
-            "delivery_fee": 20,
+            "delivery_fee": "6.20",
             // "image": "https://via.placeholder.com/300/09f/fff.png"
         },
         {
-            "id": "002",
+            // "id": "002",
             "restaurant_name": "The Place Restaurant",
             "description": "Redundant alt attribute. Screen-readers already announce `img` tags as an image. You don’t need to use the words `image`, `photo,` or `picture` (or any specified custom words) in the alt prop",
             "rating": "4.8",
-            "delivery_fee": 20,
+            "delivery_fee": "20.00",
             // "image": "https://via.placeholder.com/300/09f/fff.png"
         },
     ]
